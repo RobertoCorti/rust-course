2554fe263c55b64dc71040f7da1e9364051d5c02
Roberto Corti <robi290596@gmail.com>
2025-03-01 15:15:28 +0100

    Add variables folder

---
+++ variables/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // let mut x = 5;
+    // println!("The value of x is: {}", x);
+
+    // x = 1;
+    // println!("The value of x is: {}", x);
+
+    // constants
+    const MAX_POINTS:i32 = 100_000;
+    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+}
